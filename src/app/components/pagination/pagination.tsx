dde97a17b68f3d0ec462e6b9b1fc11abf6b1709b
Leanid <leanid.sushchynski@gmail.com>
2025-05-13 21:57:43 +0300

    Change Pagination and Page

--- src/app/components/pagination/pagination.tsx
+++ src/app/components/pagination/pagination.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React from "react";
+import Image from "next/image";
+import left from "../../../assets/img/left.png";
 
 type PaginationProps = {
   currentPage: number;
@@ -23,35 +25,42 @@ export default function Pagination({
   };
 
   return (
-    <div className="flex items-center justify-center gap-2 mt-6">
+    <div className="flex items-center justify-center gap-3 mt-6">
+      {/* Left Arrow */}
       <button
         onClick={handlePrev}
         disabled={currentPage === 1}
-        className="px-3 py-1 border text-sm rounded disabled:border-gray-400 disabled:opacity-50"
+        className="w-8 h-8 flex items-center justify-center  text-lg disabled:opacity-30"
       >
-        Prev
+        <Image src={left} className="w-5 h-5" alt="" />
       </button>
 
-      <div className="flex items-center gap-1">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            onClick={() => onPageChange(index + 1)}
-            className={`px-3 py-1 border rounded ${
-              currentPage === index + 1 ? "bg-blue-500 text-white" : ""
-            }`}
-          >
-            {index + 1}
-          </button>
-        ))}
+      {/* Page Numbers */}
+      <div className="flex items-center gap-2">
+        {Array.from({ length: totalPages }, (_, index) => {
+          const page = index + 1;
+          const isActive = currentPage === page;
+
+          return (
+            <button
+              key={page}
+              onClick={() => onPageChange(page)}
+              className={`text-sm h-10 w-10 rounded-full transition 
+                ${isActive ? "bg-blue-500 text-white" : "text-black"}`}
+            >
+              {page}
+            </button>
+          );
+        })}
       </div>
 
+      {/* Right Arrow */}
       <button
         onClick={handleNext}
         disabled={currentPage === totalPages}
-        className="px-3 py-1 text-sm border rounded disabled:opacity-50"
+        className="w-8 h-8 flex items-center justify-center  text-lg disabled:opacity-30"
       >
-        Next
+        <Image src={left} className="w-5 h-5 rotate-180" alt="" />
       </button>
     </div>
   );
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,71 +6,91 @@ import Image from "next/image";
 import uploadIcon from "../../assets/icons/upload.svg";
 
 export default function HotelForm() {
-  const [file, setFile] = useState<File | null>(null);
+  const [files, setFiles] = useState<File[]>([]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files?.[0]) {
-      setFile(e.target.files[0]);
+    if (e.target.files) {
+      setFiles(Array.from(e.target.files));
     }
   };
 
-  const removeImage = () => setFile(null);
+  const removeImage = (indexToRemove: number) => {
+    setFiles((prev) => prev.filter((_, index) => index !== indexToRemove));
+  };
 
   return (
     <div>
       <Navbar />
-      <div className="max-w-[648px] mx-auto px-4 md:px-0 pt-6 flex flex-col gap-6">
-        <h1 className="text-center text-2xl font-bold">Register your object</h1>
+      <div className="max-w-[648px] mx-auto px-4 md:px-0 pt-6 pb-10 flex flex-col gap-6">
+        <h1 className="text-center text-xl font-bold">Register your object</h1>
         {/* name */}
         <div className="focus-within:text-blue-700">
-          <p className="pb-2 text-lg transition-colors text-inherit">
+          <p className="pb-2 text-base transition-colors text-inherit">
             Name of object
           </p>
           <input
             type="text"
             placeholder="Name of object"
-            className="w-full h-14 px-4 text-lg border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all"
+            className="w-full h-14 px-4 text-base border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all"
           />
         </div>
 
         {/* number */}
         <div className="focus-within:text-blue-700">
-          <p className="pb-2 text-lg transition-colors text-inherit">Number </p>
+          <p className="pb-2 text-base transition-colors text-inherit">
+            Number{" "}
+          </p>
           <input
             type="text"
             placeholder="+48 999-23-23"
-            className="w-full h-14 px-4 text-lg border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all"
+            className="w-full h-14 px-4 text-base border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all"
           />
         </div>
 
         {/* geo */}
         <div className="focus-within:text-blue-700">
-          <p className="pb-2 text-lg transition-colors text-inherit">
+          <p className="pb-2 text-base transition-colors text-inherit">
             Location{" "}
           </p>
           <input
             type="text"
             placeholder="Lublin"
-            className="w-full h-14 px-4 text-lg border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all"
+            className="w-full h-14 px-4 text-base border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all"
           />
         </div>
         {/* Info */}
         <div className="focus-within:text-blue-700">
-          <p className="pb-2 text-lg transition-colors text-inherit">Info</p>
+          <p className="pb-2 text-base transition-colors text-inherit">Info</p>
           <textarea
             placeholder="Opis (opcjonalnie)"
-            className="w-full h-32 px-4 pt-4 text-lg text-black border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all resize-none"
+            className="w-full h-32 px-4 pt-4 text-base text-black border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all resize-none"
           />
         </div>
-        <div className="focus-within:text-blue-700">
-          <p className="pb-2 text-lg transition-colors text-inherit">
+        <div className="focus-within:text-blue-700 relative  w-full">
+          <p className="pb-2 text-base transition-colors text-inherit">
             Select type of object
           </p>
-          <select className="w-full h-14 px-4 text-lg border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 transition-all">
+          <select className="w-full  h-14 pr-12 pl-4 text-base border border-gray-300 text-black rounded-full focus:outline-none focus:ring-2 focus:ring-blue-700 appearance-none transition-all">
             <option value="room">Room</option>
             <option value="hotel">Hotel</option>
             <option value="other">Other</option>
           </select>
+
+          <div className="pointer-events-none absolute top-[70%] right-4 transform -translate-y-1/2">
+            <svg
+              className="w-5 h-5 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
         </div>
 
         {/* upload*/}
@@ -78,7 +98,7 @@ export default function HotelForm() {
           htmlFor="fileInput"
           className="w-full border-2 border-dashed border-blue-500 rounded-lg py-8 flex flex-col items-center justify-center text-center cursor-pointer hover:bg-blue-50 transition"
         >
-          {!file && (
+          {files.length === 0 && (
             <>
               <Image
                 src={uploadIcon}
@@ -93,26 +113,36 @@ export default function HotelForm() {
               </p>
             </>
           )}
+
           <input
             id="fileInput"
             type="file"
+            multiple
             accept="image/*"
             onChange={handleFileChange}
             className="hidden"
           />
-          {file && (
-            <div className="flex flex-col items-center">
-              <p className="text-gray-600 text-sm">{file.name}</p>
-              <button
-                type="button"
-                className="text-main-blue text-sm font-bold hover:underline mt-2"
-                onClick={removeImage}
-              >
-                Delete
-              </button>
+
+          {files.length > 0 && (
+            <div className="flex flex-col gap-2 items-center">
+              {files.map((file, index) => (
+                <div key={index} className="flex items-center gap-2">
+                  <p className="text-gray-600 text-sm">{file.name}</p>
+                  <button
+                    type="button"
+                    className="text-main-blue text-sm font-bold hover:underline"
+                    onClick={() => removeImage(index)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
             </div>
           )}
         </label>
+        <button className="bg-blue-600 hover:bg-blue-800 duration-200 text-white text-base px-14 py-3 rounded-full">
+          Register
+        </button>
       </div>
     </div>
   );
